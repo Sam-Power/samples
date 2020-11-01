20183414295869f67ec4ea852722c9c843078541
Sam <samyildiz96@gmail.com>
2020-11-01 13:21:36 +0100

    LC

---
+++ LC_58. Length of Last Word.py
@@ -0,0 +1,32 @@
+"""
+58. Length of Last Word
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a maximal substring consisting of non-space characters only.
+
+Example:
+
+Input: "Hello World"
+Output: 5
+
+"""
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return 0 if len(s.split()) == 0 else len(s.split()[-1])
+
+s = "Hello World"
+print(Solution().lengthOfLastWord(s))
+
+
+
+
+
+
+
+
+
+
+
+
